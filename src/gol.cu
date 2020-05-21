2ffbca1767f543fa7faa031f20103b4a00d76c3a
Logan Apple <loganapple@hotmail.com>
2020-05-21 13:50:32 -0400

    Continued working on texture memory.

--- src/gol.cu
+++ src/gol.cu
@@ -8,13 +8,15 @@
 
 // Trying texture memory -- probably not better than just using shared memory,
 // but it's worth a shot.
-texture<uint8_t, 2> texmem;
+// Also, I feel like this shouldn't be global, but all the CUDA examples have
+// it as such?
+texture<uint8_t, 2, cudaReadModeElementType> texmem;
 
 // What if I just passed the grid instead?
-__host__ __device__ int count_neighbors(int x, int y, 
+__host__ __device__ uint8_t count_neighbors(int x, int y, 
                                         int width, int height, 
                                         uint8_t* cells) {
-    int alive = 0;
+    uint8_t alive = 0;
     
     if (x < 0 || x >= width || y < 0 || y >= height) {
         return 0;
@@ -44,7 +46,7 @@ __global__ void naive_update_kernel(int width, int height,
 
     for (; tidy < height; tidy += num_threads_y) {
         for (; tidx < width; tidx += num_threads_x) {
-            int neighbors = count_neighbors(tidx, tidy, width, height, cells);
+            uint8_t neighbors = count_neighbors(tidx, tidy, width, height, cells);
             // Any live cell with two or three neighbors survives.
             if (cells[tidy * width + tidx] == 1 && 
                     (neighbors == 2 || neighbors == 3)) {
@@ -63,57 +65,40 @@ __global__ void naive_update_kernel(int width, int height,
 }
 
 __global__ void optimized_update_kernel(int width, int height,
-                                        uint8_t* cells, uint8_t* updated_cells) {
-    // const int num_threads = blockDim.x * blockDim.y;
-    // Also assuming 16 for right now, can't use variables to declare the shmem.
-    // Currently doing one cell per thread.
-    __shared__ uint8_t shmem[16 + 2][16 + 2];
-
+                                        uint8_t* updated_cells) {
     int tidx = blockIdx.x * blockDim.x + threadIdx.x;
     int tidy = blockIdx.y * blockDim.y + threadIdx.y;
-    int i = threadIdx.y;
-    int j = threadIdx.x;
 
-    if (tidx < width && tidy < height) {
-        shmem[i][j] = cells[tidy * width + tidx];
-    }
-
-    __syncthreads();
-
-    if (tidx < width && tidy < height) {
-        if (i > 0 && i < blockDim.y && j > 0 && j < blockDim.x) {
-            int neighbors = 0;
+    uint8_t neighbors = 0;
             
-            // Take advantage of loop unrolling to make this faster.
-            #pragma unroll
-            for (int x = -1; x <= 1; ++x) {
-                #pragma unroll
-                for (int y = -1; y <= 1; ++y) {
-                    int y2 = i + y;
-                    int x2 = j + x;
-                    if (y2 != y || x2 != x) {
-                        if (y2 >= 0 && y2 < height && 
-                            x2 >= 0 && x2 < width) {
-                            neighbors += shmem[y2][x2];
-                        }
-                    }
+    // Take advantage of loop unrolling to make this faster.
+    #pragma unroll
+    for (int x = -1; x <= 1; ++x) {
+        #pragma unroll
+        for (int y = -1; y <= 1; ++y) {
+            int y2 = tidy + y;
+            int x2 = tidx + x;
+            if (y2 != y || x2 != x) {
+                if (y2 >= 0 && y2 < height && 
+                    x2 >= 0 && x2 < width) {
+                    neighbors += tex2D(texmem, x2, y2);
                 }
             }
-
-            // Any live cell with two or three neighbors survives.
-            if (shmem[i][j] == 1 && 
-                    (neighbors == 2 || neighbors == 3)) {
-                updated_cells[tidy * width + tidx] = 1;
-            }
-            // Any dead cell with three live neighbors comes to life.
-            else if (shmem[i][j] == 0 && neighbors == 3) {
-                updated_cells[tidy * width + tidx] = 1;
-            }
-            // Any other cells die.
-            else {
-                updated_cells[tidy * width + tidx] = 0;
-            }
         }
+    }
+
+    // Any live cell with two or three neighbors survives.
+    if (tex2D(texmem, tidx, tidy) == 1 && 
+            (neighbors == 2 || neighbors == 3)) {
+        updated_cells[tidy * width + tidx] = 1;
+    }
+    // Any dead cell with three live neighbors comes to life.
+    else if (tex2D(texmem, tidx, tidy) == 0 && neighbors == 3) {
+        updated_cells[tidy * width + tidx] = 1;
+    }
+    // Any other cells die.
+    else {
+        updated_cells[tidy * width + tidx] = 0;
     }
 }
 
@@ -126,8 +111,14 @@ void call_cuda_gol_update(int num_threads,
     dim3 grid_size(int((width + num_threads - 1) / num_threads), 
                    int((height + num_threads - 1) / num_threads));
     if (optimized) {
+        cudaChannelFormatDesc desc = cudaCreateChannelDesc<uint8_t>();
+
+        size_t pitch;
+        cudaMallocPitch(&cells, &pitch, sizeof(uint8_t) * width, height);
+        cudaBindTexture2D(0, texmem, cells, desc, width, height, pitch);
+
         optimized_update_kernel<<<grid_size, block_size>>>(width, height, 
-            cells, updated_cells);
+            updated_cells);
     }
     else {
         naive_update_kernel<<<grid_size, block_size>>>(width, height, 
